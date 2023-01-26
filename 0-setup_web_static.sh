ef17eed5166d3d6abc3591595326140de3d93f95
Yofty <106748943+Yofty@users.noreply.github.com>
2023-01-26 19:24:55 +0000

    correction

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -10,10 +10,12 @@ echo "<!DOCTYPE html>
   <head>
   </head>
   <body>
-    <p>Nginx server test</p>
+    Holberton School
   </body>
 </html>" | tee /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data
-sudo sed -i '39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}n'/etc/nginx/sites-enabled/default
+sudo sed -i '39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}n' /etc/nginx/sites-enabled/default
+sudo fuser -k 80/tcp
+sudo fuser -k 443/tcp
 sudo service nginx restart
