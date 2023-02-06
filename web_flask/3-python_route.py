677d074641004deb6cdaa957c54653893b82d835
Yofty <106748943+Yofty@users.noreply.github.com>
2023-02-06 11:12:11 +0000

    correction1

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -22,7 +22,7 @@ def c_text(text):
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<string:text>', strict_slashes=False)
-def python_text(text):
+def python_text(text='is cool'):
     text = text.replace("_", " ")
     return "python %s" % text
 
